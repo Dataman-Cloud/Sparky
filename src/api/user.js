cee730f652bff6d745fa3158d0166c37df9a963f
my9074 <313231068@qq.com>
2017-04-06 14:10:15 +0800

    1.Update .gitignore 2.Update package.json 3.Add Login vue 4.Init user store 5.Modify axios http intercepter

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+/**
+ * Created by my9074 on 2017/4/6.
+ */
+import axios from './http'
+
+export function login (data) {
+  return axios.post(`v1/login`, data)
+}
+
+export function logout () {
+  return axios.post(`v1/logout`)
+}
