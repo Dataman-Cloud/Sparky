8da681e44d4b9513a46742b4a02e04746c51f35e
webStorm <webStorm@webStorm.com>
2017-07-11 10:43:08 +0800

    修复细节

--- src/views/app/ModelList/ModelList.vue
+++ src/views/app/ModelList/ModelList.vue
@@ -12,7 +12,7 @@
       </el-form>
     </el-col>
     <!--列表-->
-    <el-table :data="filterModels" highlight-current-row v-loading="listLoading" style="width: 100%;">
+    <el-table :data="modelList" highlight-current-row v-loading="listLoading" style="width: 100%;">
       <el-table-column prop="name" label="模版名称" min-width="65" sortable  show-overflow-tooltip>
       </el-table-column>
       <el-table-column prop="accountsName" label="创建者" width="100" sortable >
@@ -88,7 +88,8 @@
           json: ''
         },
         prefix: LABEL_PREFIX,
-        interval: null
+        interval: null,
+        modelList: []
       }
     },
     watch: {
@@ -121,7 +122,7 @@
         // 从已有数组中返回元素
         if (this.models && Array.isArray(this.models) && this.models.length > 0) {
           // 获取列表数据
-          let modelList = this.models.slice((this.page - 1) * this.pageSize, this.page * this.pageSize)
+          let ml = this.models.slice((this.page - 1) * this.pageSize, this.page * this.pageSize)
           /* -- 判断权限 -- */
           // 当前登录用户的当前组信息
           let groups = []
@@ -143,7 +144,7 @@
             }
           }
           // 循环列表数据 判断权限
-          for (let v of modelList) {
+          for (let v of ml) {
             // 默认没有权限
             v['isRole'] = false
             if (group.role === 'superuser') { // 超管有权限
@@ -156,9 +157,8 @@
               v['isRole'] = true
             }
           }
-          return modelList
+          this.modelList = ml
         } else {
-          return []
         }
       }
     },
@@ -203,7 +203,9 @@
       listModel () {
         return this.$store.dispatch(type.FETCH_SELECT_MODEL, {
           curGroupId: this.curGroupId
-        }).then(() => {})
+        }).then(() => {
+          this.filterModels
+        })
       },
       // 添加新的应用模版
       addAppModel () {
