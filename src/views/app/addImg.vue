0eacbe033ff21bd13ba7a9df81cb88dda943ebaa
zhaocc90 <cczhao@dataman-inc.com>
2017-07-13 09:56:53 +0800

    无更改

--- src/views/app/addImg.vue
+++ src/views/app/addImg.vue
@@ -268,7 +268,7 @@
 
     <el-form-item>
       <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-      <!--<el-button @click="resetForm('ruleForm')">重置</el-button>-->
+      <!-- <el-button @click="resetForm('ruleForm')">重置</el-button> -->
     </el-form-item>
 
   </el-form>
